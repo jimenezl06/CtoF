a340133ac19d4624f4c6f1680f3dcdbcc23b22ae
jimenezl06 <jimenez.l06@mymail.sxu.edu>
2025-02-22 20:38:17 -0600

    Create programonee.py
    Converts Celsius to Fahrenheit

---
+++ programonee.py
@@ -0,0 +1,16 @@
+#Luis Jimenez
+
+#This program will convert a temperature in Centigrade to a temperature in Fahrenheit
+
+#Input
+#This asks for user input on the Celsius Temperature
+temp_Cent = int(input("Enter your Centigrade Temperature: "))
+
+#Process
+#This is the formula
+temp_Fahr = temp_Cent*9/5+32
+
+#Output
+#It prints out the temperature in Fahrenheit
+print("Your input in Fahrenheit is")
+print(temp_Fahr)
